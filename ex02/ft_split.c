67a93adac8267df67c9034a3e5a7a275d39ec931
Gaylor Laurent <galauren@made-f0Cr4s5.clusters.42paris.fr>
2025-01-29 19:05:02 +0100

    split il st finit mais norme

--- ex02/ft_split.c
+++ ex02/ft_split.c
@@ -2,117 +2,119 @@
 
 int   is_delimiter(char c, char delimiter)
 {
-	if (c == delimiter)
-		return (1);
-	return (0);
+		if (c == delimiter)
+				return (1);
+		return (0);
 }
 
 int   count_words(const char *str, char delimiter)
 {
-	int   count;
-	int   in_word;
+		int   count;
+		int   in_word;
 
-	count = 0;
-	in_word = 0;
-	while (*str)
-	{
-		if (!is_delimiter(*str, delimiter) && in_word == 0)
+		count = 0;
+		in_word = 0;
+		while (*str)
 		{
-			in_word = 1;
-			count++;
+				if (!is_delimiter(*str, delimiter) && in_word == 0)
+				{
+						in_word = 1;
+						count++;
+				}
+				else if (is_delimiter(*str, delimiter))
+						in_word = 0;
+				str++;
 		}
-		else if (is_delimiter(*str, delimiter))
-			in_word = 0;
-		str++;
-	}
-	return (count);
+		return (count);
 }
 
 char  *copy_word(const char *start, const char *end)
 {
-	char  *word;
-	int         i;
+		char  *word;
+		int         i;
 
-	word = malloc(sizeof(char) * (end - start + 1));
-	if (!word)
-		return (0);
-	i = 0;
-	while (start < end)
-	{
-		word[i] = *start;
-		start++;
-		i++;
-	}
-	word[i] = '\0';
-	return (word);
+		word = malloc(sizeof(char) * (end - start + 1));
+		if (!word)
+				return (0);
+		i = 0;
+		while (start < end)
+		{
+				word[i] = *start;
+				start++;
+				i++;
+		}
+		word[i] = '\0';
+		return (word);
 }
 
 int   fill_result(const char *str, char delimiter, char **result)
 {
-	const char  *start;
-	int               i;
+		const char  *start;
+		int               i;
 
-	i = 0;
-	while (*str)
-	{
-		if (!is_delimiter(*str, delimiter))
+		i = 0;
+		while (*str)
 		{
-			start = str;
-			while (*str && !is_delimiter(*str, delimiter))
-				str++;
-			result[i] = copy_word(start, str);
-			if (!result[i])
-				return (-1);
-			i++;
+				if (!is_delimiter(*str, delimiter))
+				{
+						start = str;
+						while (*str && !is_delimiter(*str, delimiter))
+								str++;
+						result[i] = copy_word(start, str);
+						if (!result[i])
+								return (-i);
+						i++;
+				}
+				else
+						str++;
 		}
-		else
-			str++;
-	}
-	result[i] = 0;
-	return (0);
+		result[i] = 0;
+		return (0);
 }
 
 char  **ft_split(const char *str, char delimiter)
 {
-	char  **result;
-	int         words;
+		char  **result;
+		int     words;
+		int		nb_to_free;
 
-	if (!str)
-		return (0);
-	words = count_words(str, delimiter);
-	result = malloc(sizeof(char *) * (words + 1));
-	if (!result)
-		return (0);
-	if (fill_result(str, delimiter, result) == -1)
-	{
-		while (words--)
-			free(result[words]);
-		free(result);
-		return (0);
-	}
-	return (result);
+		if (!str)
+				return (0);
+		words = count_words(str, delimiter);
+		result = malloc(sizeof(char *) * (words + 1));
+		if (!result)
+				return (0);
+		nb_to_free = fill_result(str, delimiter, result);
+		if (nb_to_free < 0)
+		{
+				nb_to_free *= -1;
+				while (nb_to_free--)
+						free(result[nb_to_free]);
+				free(result);
+				return (0);
+		}
+		return (result);
 }
-
+/*
 #include <stdio.h>
-
 int   main(void)
 {
-	char  **result;
-	int         i;
+		char  **result;
+		int         i;
 
-	result = ft_split("hello world this is C", ' ');
-	if (!result)
-	{
-		printf("Error: Memory allocation failed\n");
-		return (1);
-	}
-	i = 0;
-	while (result[i])
-	{
-		printf("Word %d: %s\n", i, result[i]);
-		free(result[i]);
-		i++;
-	}
-	free(result);
-	return (0);
-}
+		result = ft_split("hello world this is C", ' ');
+		if (!result)
+		{
+				printf("Error: Memory allocation failed\n");
+				return (1);
+		}
+		i = 0;
+		while (result[i])
+		{
+				printf("Word %d: %s\n", i, result[i]);
+				free(result[i]);
+				i++;
+		}
+		free(result);
+		return (0);
+}*/
